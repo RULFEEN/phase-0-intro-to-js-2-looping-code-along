078cdde998f45eafc3e54811a83ec1d55c831ddb
Victor Mayi <victormay.i.797@gmail.com>
2022-08-12 19:36:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }*/
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  
+    return gifts;
+}
+  
+wrapGifts(gifts);
+*/
+/*
+const cards = ["Ada", "Brendan", "Ali"];
+const birthday = "the wonderful birthday gift!"
+
+function writeCards([cards], [birthday]) {
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for ${birthday}`);
+        writtenCards = {...cards};
+    }
+    return writtenCards;
+}
+writeCards([cards], [birthday]);
+*/
+
+function countDown() {
+    let int = 20;
+    while (int >= 0) {
+        console.log(`I am ${int}, a positive integer!`);
+        int--;
+    }
+    return int;
+}
+countDown();
